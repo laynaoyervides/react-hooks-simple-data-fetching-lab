9babe7a4a8d6b3a1be633631c4f98f31cdf17b85
Layna Oyervides <laynaoyervides@gmail.com>
2021-12-30 20:39:50 -0700

    commit tree 2b4dff14d464fd53101f07eb12e95a81849a3f98

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect,} from "react";
+
+function App () {
+    const [image, setImage] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+     useEffect (()=>{
+     fetch("https://dog.ceo/api/breeds/image/random")
+     .then((resp) => resp.json())
+     .then ((json) => {
+            setImage(json.message);
+            setIsLoaded(true);
+            console.log(json.message);
+
+        })
+    }, []);
+     if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+    <img alt='A Random Dog' src={image}></img>
+)
+};
+
+export default App;
